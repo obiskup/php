0031089647bb826008b1178b22a6e5460abbe4ab
obiskup <oliwierbiskup199@gmail.com>
2025-09-04 15:15:43 +0200

    liczby całkowite

---
+++ liczbaCalkowita.php
@@ -0,0 +1,8 @@
+<?php
+    $a = 1234; //dziesiętna
+    $b = 0123; //ósemkowa
+    $c = 0o123; //ósemkowy w php 8
+    $d = 0x1A; // hex
+    $e = 0b1111111; //binarny
+    $f = 1_234_567 //dziesiętny w php 7.4
+?>
