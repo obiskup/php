5af681bc55824a767743fecc4e355ed6b5afc798
obiskup <oliwierbiskup199@gmail.com>
2025-09-08 10:43:39 +0200

    printf

---
+++ printf.php
@@ -0,0 +1,9 @@
+<?php
+    $tekst = "Hello";
+    printf("[%s]\n", $tekst); // [Hello]
+    printf("[%10s]\n", $tekst);
+    printf("[%-10s]\n", $tekst);
+    printf("[%.3s]\n", $tekst);
+    printf("[%10.3s]\n", $tekst);
+    printf("[%-10.3s]\n", $tekst);
+?>
