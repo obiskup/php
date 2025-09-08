97987b29ea3fe982229f4a5615eb1a2e0944bee7
obiskup <oliwierbiskup199@gmail.com>
2025-09-08 11:07:09 +0200

    printf liczby

---
+++ printf_int.php
@@ -0,0 +1,11 @@
+<?php
+    $n = "43951789";
+
+    printf("%%d = '%d'<br>", $n);
+    printf("%%f = '%f'<br>", $n);
+    printf("%%e = '%e'<br>", $n);
+    printf("%%b = '%b'<br>", $n);
+    printf("%%o = '%o'<br>", $n);
+    printf("%%s = '%s'<br>", $n);
+    printf("%%x = '%x'<br>", $n);
+?>
