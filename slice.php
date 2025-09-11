9f28e520464eab1d59f21643c6fc839162809583
obiskup <oliwierbiskup199@gmail.com>
2025-09-11 14:42:03 +0200

    Sliceeeeee

--- slice.php
+++ slice.php
@@ -1,4 +1,6 @@
 <?php
     $a=array("red","green","blue","yellow","brown");
-    print_r(array_slice($a,2));
+    print_r($a); echo "<br>";
+    print_r(array_slice($a,2)); echo "<br>";
+    print_r($a);
 ?>
