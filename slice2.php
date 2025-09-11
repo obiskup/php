a37958a94debe237b1c1c31d69a41a8ae4a28b80
obiskup <oliwierbiskup199@gmail.com>
2025-09-11 14:43:42 +0200

    Slice 2

---
+++ slice2.php
@@ -0,0 +1,6 @@
+<?php
+    $a=array("red","green","blue","yellow","brown");
+    print_r($a); echo "<br>";
+    print_r(array_slice($a,1,2)); echo "<br>"; // zaczyna slice od drugiego elementu (1), i zwraca tylko dwa elementy
+    print_r($a);
+?>
