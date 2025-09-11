080b9c56b2f277e32acd9901303825b8e618821d
obiskup <oliwierbiskup199@gmail.com>
2025-09-11 12:47:56 +0200

    Tablice indexowane

---
+++ tablice_indexowane.php
@@ -0,0 +1,12 @@
+<?php
+    $cars1 = array("Volvo", "BMW", "Toyota");
+    
+    $cars2 = ["Volvo", "BMW", "Toyota"];
+
+    $cars3 = [];
+    $cars3[0] = "Volvo";
+    $cars3[1] = "BMW";
+    $cars3[2] = "Toyota";
+
+    echo "$cars1[0], $cars2[1], $cars3[2]"
+?>
