bb735ec738f69d0289a80b571ce7daece78af257
obiskup <oliwierbiskup199@gmail.com>
2025-09-15 10:38:00 +0200

    operator arytmetkykykyakaydkyadk

---
+++ operator_arytmetyczne.php
@@ -0,0 +1,11 @@
+<?php
+    $a=5;
+    $b=3;
+    echo "liczby a=$a, liczba b=$b <br>"; //8
+    echo "suma a+b=".($a+$b)."<br>"; //2
+    echo "różnica a-b=".($a-$b)."<br>"; //15
+    echo "iloczyn a*b=".($a*$b)."<br>"; //1.6666
+    echo "iloraz a/b=".($a/$b)."<br>"; //2
+    echo "modulo a%b=".($a%$b)."<br>"; //125
+    echo "aq do potęgi b = ".$a**$b;
+?>
