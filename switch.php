c427f4a7788fa705264be63390627daf4e87c666
obiskup <oliwierbiskup199@gmail.com>
2025-09-18 12:54:21 +0200

    Switch 2

--- switch.php
+++ switch.php
@@ -1,11 +1,17 @@
 <?php
-    $favcolor = "red";
+    $favcolor = "yellow";
     switch ($favcolor) {
         case "red":
             echo "Your favorite color is red!";
             break;
         case "blue":
             echo "Your favorite color is blue!";
+            break;
+        case "green":
+            echo "Your favorite color is green!";
+            break;
+        case "yellow":
+            echo "Your favorite color is yellow!";
             break;
         default:
             echo "Your favorite color is neither red or blue!";
