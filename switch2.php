762319d106577c39679be0dda29b46b753d27adf
obiskup <oliwierbiskup199@gmail.com>
2025-09-18 13:02:56 +0200

    Switch

---
+++ switch2.php
@@ -0,0 +1,13 @@
+<?php
+    $i = 3;
+    switch ($i) {
+        case 0:
+        case 1:
+        case 2:
+            echo "i is less than 3 but not negative";
+            break;
+        case 3:
+            echo "i is 3";
+            break;
+    }
+?>
