9bc0572f79eea82cc1b8c066ead84e0fbaf152c1
obiskup <oliwierbiskup199@gmail.com>
2025-09-18 12:49:48 +0200

    Switch

---
+++ switch.php
@@ -0,0 +1,13 @@
+<?php
+    $favcolor = "red";
+    switch ($favcolor) {
+        case "red":
+            echo "Your favorite color is red!";
+            break;
+        case "blue":
+            echo "Your favorite color is blue!";
+            break;
+        default:
+            echo "Your favorite color is neither red or blue!";
+    }
+?>
