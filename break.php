91cf689a02f8501bd558346541fc99c12b78af43
obiskup <oliwierbiskup199@gmail.com>
2025-09-22 10:29:49 +0200

    Lekcja 1 22.09.2025

---
+++ break.php
@@ -0,0 +1,9 @@
+<?php
+    for($i=0; $i<10; $i++){
+        echo "liczba: $i <br>\n";
+        if($i == 5){
+            break;
+        }
+    }
+    echo "Koniec pętli";
+?>
---
+++ goto.php
@@ -0,0 +1,7 @@
+<?php
+    goto a;
+    echo "pierwszy tekst";
+
+    a:
+        echo "drugi tekst";
+?>
