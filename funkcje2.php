ec6c4468768354471945c746d41975ee2740ac88
obiskup <oliwierbiskup199@gmail.com>
2025-09-22 11:40:27 +0200

    Lekcja 2 22.09.2025

---
+++ funkcje2.php
@@ -0,0 +1,8 @@
+<?php
+    function familyName($fname){
+        echo "$fname Refsnes.<br>";
+    }
+    familyName("Jani");
+    familyName("Hege");
+    familyName("Stale");
+?>
---
+++ funkcje_anonimowe.php
@@ -0,0 +1,3 @@
+<?php
+    echo preg_replace_callback('~-([a-z])~', function($match) {return strtoupper($match[1]);}, 'hello-world');
+?>
